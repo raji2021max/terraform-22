e953f8bb3a2d0f58544fa590aa39e04a8630d43b
raji <raji.veera2003@gmail.com>
2025-02-20 15:55:20 +0530

    add

---
+++ day2-basic-code/terraform.tfvars
@@ -0,0 +1,3 @@
+ami_id= "ami-053a45fff0a704a47"
+instance_type= "t2.micro"
+keypair = "empty"
---
+++ day2-basic-code/variable.tf
@@ -0,0 +1,23 @@
+variable ami_id {
+  type        = string
+  default     = ""
+}
+
+
+ variable instance_type {
+   type        = string
+   default     = ""
+ }
+ 
+
+ 
+
+ variable keypair {
+   type        = string
+   default     = ""
+   description = "description"
+ }
+
+ 
+ 
+ 
